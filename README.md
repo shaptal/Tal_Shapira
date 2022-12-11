9b2c437595c8946555c2f388bf3ca448c8aa3f8f
shaptal <shaptal@post.BGU.ac.il>
2022-12-11 17:17:34 +0200

    Task2

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Tal_Shapira
 Hi Naama, My name is Tal Shapira
+
+Task 2 Includes changes from part 1.
+
+The page will not include Flight Plan and will only Analize live weather data.
---
+++ TASK 2/static/AirMass.js
@@ -0,0 +1,13 @@
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a').forEach(link => {    
+    if(link.href.includes(`${activePage}`)){
+      link.classList.add('active');
+    }
+  });
+  
+
+var d = new Date();
+	var n = d.getHours();
+	if (n > 18 || n < 6)
+	  // If time is after 19PM or before 6AM, apply night theme to ‘body’
+	  document.body.className = "night";
